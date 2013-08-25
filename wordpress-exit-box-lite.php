f6430ac488c44e6ba1c430e6febff55e760e0391
angrybyte <angrybyte@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-25 17:47:43 +0000

    Confirmed Compatibility with 3.6
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-exit-box-lite/trunk@762059 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-exit-box-lite.php
+++ wordpress-exit-box-lite.php
@@ -3,14 +3,14 @@
 /**
  * @package Wordpress Exit Box Lite
  * @author Bouzid Nazim Zitouni
- * @version 1.10
+ * @version 1.11
  */
 /*
 Plugin Name: Wordpress Exit Box Lite
 Plugin URI: http://angrybyte.com/wordpress-plugins/wordpress-exit-box-finally-released/
 Description: Wordpress exit box is used design and display your exit box, a ThickBox page that will be shown to your users when they click a external link.
 Author: Bouzid Nazim Zitouni
-Version: 1.10
+Version: 1.11
 Author URI: http://angrybyte.com
 */
 
